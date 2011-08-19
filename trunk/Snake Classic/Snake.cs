a4e5e37b41009091032eb3ce223f9d0dd96132f0
gmmateev92@abv.bg <gmmateev92@abv.bg@c3d3e4a3-63af-dc45-4941-66f2009401bc>
2011-08-19 21:16:35 +0000

    Upload project

---
+++ trunk/Snake Classic/Snake.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake_Classic
+{
+    public enum Directions
+    {
+        Left,Right,Up,Down
+    }
+
+    class Snake
+    {
+        Directions direction;
+        Queue<SnakeFragment> snakeBody;
+
+        public Directions Direction
+        {
+            get { return direction; }
+            set { direction = value; }
+        }
+
+        public Snake()
+        {
+            Direction = Directions.Left;
+        }
+        
+    }
+
+    public class SnakeFragment
+    {
+        int row;
+        int col;
+
+        public int Row
+        {
+            get { return row; }
+            set { row = value; }
+        }     
+        public int Col
+        {
+            get { return col; }
+            set { col = value; }
+        }
+
+        public SnakeFragment(int row, int col)
+        {
+            this.Row = row;
+            this.Col = col;
+            PutIntoMatrix(this.Row, this.Col);
+        }
+
+        
+    }
+}
